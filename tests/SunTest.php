5cd156318c4fd4ce8454cd4d4b2d20f383003693
freek <freek@spatie.be>
2020-12-08 10:25:56 +0100

    use expectation api

--- tests/SunTest.php
+++ tests/SunTest.php
@@ -17,34 +17,34 @@ beforeEach(function () {
 });
 
 it('can determine sunrise, zenith and sunset for the current moment', function () {
-    assertEquals('2020-01-01 08:46:41', $this->sun->sunrise()->format('Y-m-d H:i:s'));
+    expect($this->sun->sunrise()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 08:46:41');
 
-    assertEquals('2020-01-01 12:45:42', $this->sun->zenith()->format('Y-m-d H:i:s'));
+    expect($this->sun->zenith()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 12:45:42');
 
-    assertEquals('2020-01-01 16:44:43', $this->sun->sunset()->format('Y-m-d H:i:s'));
+    expect($this->sun->sunset()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 16:44:43');
 });
 
 it('can determine if the sun is up now', function () {
     TestTime::freeze($this->sun->sunrise());
     TestTime::subSecond();
-    assertFalse($this->sun->sunIsUp());
+    expect($this->sun->sunIsUp())->toBeFalse();
 
     TestTime::addSecond();
-    assertTrue($this->sun->sunIsUp());
+    expect($this->sun->sunIsUp())->toBeTrue();
 
     TestTime::freeze($this->sun->sunset());
-    assertTrue($this->sun->sunIsUp());
+    expect($this->sun->sunIsUp())->toBeTrue();
 
     TestTime::addSecond();
-    assertFalse($this->sun->sunIsUp());
+    expect($this->sun->sunIsUp())->toBeFalse();
 });
 
 it('can determine sunrise, zenith and sunset for a specified moment', function () {
     $now = Carbon::create(2020, 4, 23, 11, 20, 0, 'Europe/Brussels');
 
-    assertEquals('2020-04-23 06:29:07', $this->sun->sunrise($now)->format('Y-m-d H:i:s'));
+    expect($this->sun->sunrise($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 06:29:07');
 
-    assertEquals('2020-04-23 13:40:37', $this->sun->zenith($now)->format('Y-m-d H:i:s'));
+    expect($this->sun->zenith($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 13:40:37');
 
-    assertEquals('2020-04-23 20:52:07', $this->sun->sunset($now)->format('Y-m-d H:i:s'));
+    expect($this->sun->sunset($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 20:52:07');
 });
