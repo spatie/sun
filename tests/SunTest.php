a24f3a2bdcefd579b9ef1f23f36c211f43bf6acd
freek <freek@spatie.be>
2020-12-11 21:03:01 +0100

    wip

--- tests/SunTest.php
+++ tests/SunTest.php
@@ -16,13 +16,6 @@ beforeEach(function () {
     $this->sun = new Sun($coordinatesOfAntwerp['lat'], $coordinatesOfAntwerp['lng']);
 });
 
-it('can determine sunrise, zenith and sunset for the current moment', function () {
-    expect($this->sun->sunrise()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 08:46:41');
-
-    expect($this->sun->zenith()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 12:45:42');
-
-    expect($this->sun->sunset()->format('Y-m-d H:i:s'))->toEqual('2020-01-01 16:44:43');
-});
 
 it('can determine if the sun is up now', function () {
     TestTime::freeze($this->sun->sunrise());
@@ -37,14 +30,4 @@ it('can determine if the sun is up now', function () {
 
     TestTime::addSecond();
     expect($this->sun->sunIsUp())->toBeFalse();
-});
-
-it('can determine sunrise, zenith and sunset for a specified moment', function () {
-    $now = Carbon::create(2020, 4, 23, 11, 20, 0, 'Europe/Brussels');
-
-    expect($this->sun->sunrise($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 06:29:07');
-
-    expect($this->sun->zenith($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 13:40:37');
-
-    expect($this->sun->sunset($now)->format('Y-m-d H:i:s'))->toEqual('2020-04-23 20:52:07');
 });
